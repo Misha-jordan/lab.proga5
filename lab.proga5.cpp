6722f26bb916e462746b0c621b80f80813caf8c9
Misha-jordan <132101205+Misha-jordan@users.noreply.github.com>
2024-05-21 15:30:50 +0300

    Update lab.proga5.cpp

--- lab.proga5.cpp
+++ lab.proga5.cpp
@@ -1,10 +1,11 @@
-﻿
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include "Header.h"
 
-using namespace std;
+
 void add_func();
 void view_func();
 void delete_func();
@@ -12,32 +13,32 @@ void search_func(std::string key);
 void edit_func();
 void menu();
 
-int main() {
-    setlocale(LC_ALL, "Russian");
-    // Пример использования функций
-    string word, line;
-    unsigned cnt {};
-    cout<<"Введите слово, число которого вы хотите посчитать" <<"\n";
-    cin>>word;
-    ifstream file("laba.txt");
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    std::string word, line;
+    unsigned cnt{};
+    std::cout << "Введите слово, кол-во которого вы хотите посчитать: " << "\n";
+    std::cin >> word;
+    std::ifstream file("file1.txt");
 
-     if (file.is_open()) {
-            getline(file, line);
-        for (unsigned i {}; i <= line.length() - word.length(); ++i) {
+    if (file.is_open()) {
+        std::getline(file, line);
+        for (unsigned i{}; i <= line.length() - word.length(); ++i) {
             size_t position = line.find(word, i);
             if (position == std::string::npos) break;
             ++cnt;
             i = position + word.length() - 1;
         }
+    }
 
+    std::cout << cnt << "\n";
+    file.close();
 
-    cout << cnt << "\n ";
-         file.close();
-
-     matrix<float> mat(3, 3);
+    matrix<float> mat(3, 3);
     mat.random();
 
-    std::ofstream f("laba(1).txt");
+    std::ofstream f("file2.txt");
 
     if (f.is_open()) {
         f << mat;
@@ -49,19 +50,20 @@ int main() {
     menu();
 }
 
+
 void menu() {
     unsigned choice = 0;
     while (choice != 6) {
-        cout << "Выберите для дальнейшего действия: " << "\n";
-        cout << "1 - Добавить новую запись в файл" << "\n";
-        cout << "2 - Просмотр всех существующих записей в файле" << "\n";
-        cout << "3 - Удаление записи из файла" << "\n";
-        cout << "4 - Поиск записи в файле" << "\n";
-        cout << "5 - Редактирование записи в файле" << "\n";
-        cout << "6 - Выход из меню" << "\n";
-        cin >> choice;
+        std::cout << "Выберите для дальнейшего действия: " << "\n";
+        std::cout << "1 - Добавить новую запись в файл" << "\n";
+        std::cout << "2 - Просмотр всех существующих записей в файле" << "\n";
+        std::cout << "3 - Удаление записи из файла" << "\n";
+        std::cout << "4 - Поиск записи в файле" << "\n";
+        std::cout << "5 - Редактирование записи в файле" << "\n";
+        std::cout << "6 - Выход из меню" << "\n";
+        std::cin >> choice;
 
-        string key;
+        std::string key;
         switch (choice)
         {
         case 1:
@@ -73,9 +75,9 @@ void menu() {
         case 3:
             delete_func();
             break;
-        case 4:            
-            cout << "Введите номер искомой записи: " << "\n";
-            cin >> key;
+        case 4:
+            std::cout << "Введите номер искомой записи: " << "\n";
+            std::cin >> key;
             search_func(key);
             break;
         case 5:
@@ -88,22 +90,22 @@ void menu() {
 }
 
 void add_func() {
-    ofstream file("laba(2).txt", std::ios_base::app);
-    string new_entry;
-    cout << "Введите новую запись: ";
-    cin >> new_entry;
+    std::ofstream file("file3.txt", std::ios_base::app);
+    std::string new_entry;
+    std::cout << "Введите новую запись: ";
+    std::cin >> new_entry;
     file << new_entry << "\n";
-    cout << "Новая запись добавлена в файл." << "\n";
+    std::cout << "Новая запись добавлена в файл." << "\n";
     file.close();
 }
 
 void view_func() {
-    ifstream file("laba(2).txt");
-    string line;
+    std::ifstream file("file3.txt");
+    std::string line;
 
     if (file.is_open()) {
-        while (getline(file, line)) {
-            cout << line << "\n";
+        while (std::getline(file, line)) {
+            std::cout << line << "\n";
         }
     }
 
@@ -111,16 +113,16 @@ void view_func() {
 }
 
 void delete_func() {
-    ifstream inFile("laba(2).txt");
-    ofstream outFile("temp.txt");
-    string line, key;
-    cout << "Введите номер записи для удаления: " << "\n";
-    cin >> key;
+    std::ifstream inFile("file3.txt");
+    std::ofstream outFile("temp.txt");
+    std::string line, key;
+    std::cout << "Введите номер записи для удаления: " << "\n";
+    std::cin >> key;
 
     if (inFile.is_open()) {
-        while (getline(inFile, line)) {
-            if (line.find(key) == string::npos) {
-                outFile << line << endl;
+        while (std::getline(inFile, line)) {
+            if (line.find(key) == std::string::npos) {
+                outFile << line << std::endl;
             }
         }
     }
@@ -128,20 +130,20 @@ void delete_func() {
     inFile.close();
     outFile.close();
 
-    remove("laba(2).txt");
-    rename("temp.txt", "laba(2).txt");
+    std::remove("file3.txt");
+    std::rename("temp.txt", "file3.txt");
 
-    cout << "" << "\n";
+    std::cout << "" << "\n";
 }
 
-void search_func(string key) {
-    ifstream file("laba(2).txt");
-    string line;
+void search_func(std::string key) {
+    std::ifstream file("file3.txt");
+    std::string line;
 
     if (file.is_open()) {
-        while (getline(file, line)) { 
-            size_t pos = line.find(key); 
-            if (pos != string::npos) {
+        while (std::getline(file, line)) {
+            size_t pos = line.find(key);
+            if (pos != std::string::npos) {
                 std::cout << line << "\n";
             }
         }
@@ -151,31 +153,31 @@ void search_func(string key) {
 }
 
 void edit_func() {
-    string key;
-    cout << "Введите номер записи для изменения: ";
-    cin >> key;
+    std::string key;
+    std::cout << "Введите номер записи для изменения: ";
+    std::cin >> key;
 
-    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-    ifstream inFile("laba(2).txt");
-    ofstream outFile("temp.txt");
+    std::ifstream inFile("file3.txt");
+    std::ofstream outFile("temp.txt");
 
     if (!inFile || !outFile) {
-        cerr << "Ошибка открытия файлов." << endl;
+        std::cerr << "Ошибка открытия файлов." << std::endl;
         return;
     }
 
-    string line;
+    std::string line;
     int currentPos = 0;
-    while (getline(inFile, line)) {
-        if (currentPos == stoi(key)) {
-            string new_entry;
-            cout << "Введите новое значение для записи: ";
-            getline(cin, new_entry);
-            outFile << new_entry << endl;
+    while (std::getline(inFile, line)) {
+        if (currentPos == std::stoi(key)) {
+            std::string new_entry;
+            std::cout << "Введите новое значение для записи: ";
+            std::getline(std::cin, new_entry);
+            outFile << new_entry << std::endl;
         }
         else {
-            outFile << line << endl;
+            outFile << line << std::endl;
         }
         currentPos++;
     }
@@ -183,10 +185,9 @@ void edit_func() {
     inFile.close();
     outFile.close();
 
-    remove("laba(2).txt");
-    rename("temp.txt", "laba(2).txt");
+    std::remove("file3.txt");
+    std::rename("temp.txt", "file3.txt");
 
-    cout << "Запись успешно отредактирована." << "\n";
+    std::cout << "Запись успешно отредактирована." << "\n";
 }
-    
     
